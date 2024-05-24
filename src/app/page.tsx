f4a2a36b6a70dd46756db3d66558b79421f201bf
Armand-SESAME <armand.nobre@sesame-digital.com>
2024-05-24 16:44:18 +0200

    :sparkles: added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import Header from "@/app/components/Header/header";
+import Footer from "@/app/components/Footer/footer";
 
 export default function Home() {
     return (
-        <main className="relative min-h-[100vw] w-full">
+        <main className="relative w-full min-h-[100vh] flex flex-col justify-between">
             <Header />
+            <div></div>
+            <Footer />
         </main>
     );
 }
